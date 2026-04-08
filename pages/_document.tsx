4286456d287f5b91702ad17c7b0776a3633d0689
Chase <chase.h.hendrickson@gmail.com>
2026-04-08 17:28:58 -0600

    Add Google Analytics G-Z57KPEE9RB

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z57KPEE9RB" />
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-Z57KPEE9RB');
+        `}} />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
